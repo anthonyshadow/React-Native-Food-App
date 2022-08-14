4369c252c724ff706bedcb32b269efc40c854a67
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-08-14 18:24:53 -0400

    setup navigation for app

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchBar = () => {
+  return <Text>Home Bar</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchBar;
