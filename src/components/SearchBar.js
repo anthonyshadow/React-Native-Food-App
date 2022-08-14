e98bb33a8c5931cdc61c2a83d5667439612cb718
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-08-14 19:02:47 -0400

    Create Search bar for app

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,40 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-const SearchBar = () => {
-  return <Text>Home Bar</Text>;
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Ionicons name="ios-search" style={styles.iconStyle} />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
+});
 
 export default SearchBar;
